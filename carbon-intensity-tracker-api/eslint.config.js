dcc9fd3f902c1b6e56528ca33a4df3cfe6c68246
Calum Brunt <mulooc@gmail.com>
2025-06-16 14:57:33 +0100

    chore(api): add prettier

--- carbon-intensity-tracker-api/eslint.config.js
+++ carbon-intensity-tracker-api/eslint.config.js
@@ -2,11 +2,12 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  { ignores: ["dist/**", "coverage/**", "node_modules/**"] }
+  { ignores: ["dist/**", "coverage/**", "node_modules/**"] },
+  prettier
 ]);
