9bfdcf93e7f6c6a2b486bf38462035e850f8d407
Calum Brunt <mulooc@gmail.com>
2025-06-16 15:26:32 +0100

    chore(ui): install eslint and add config

--- carbon-intensity-tracker-ui/eslint.config.js
+++ carbon-intensity-tracker-ui/eslint.config.js
@@ -1,28 +1,24 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig([
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      js,
+      react: pluginReact,
     },
+    languageOptions: { globals: globals.browser },
+    settings: { react: { version: "detect" } },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+    }
   },
-)
+  tseslint.configs.recommended,
+  { ignores: ["dist/**", "coverage/**", "node_modules/**"] },
+]);
+
