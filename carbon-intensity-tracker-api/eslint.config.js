88f6e2b0781aa418e4aa73bc859cdfc1b594bdce
Calum Brunt <mulooc@gmail.com>
2025-06-16 14:54:30 +0100

    chore(api): add linting to the ci workflow

--- carbon-intensity-tracker-api/eslint.config.js
+++ carbon-intensity-tracker-api/eslint.config.js
@@ -8,5 +8,5 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  { ignores: ["dist/**", "coverage/**"] }
+  { ignores: ["dist/**", "coverage/**", "node_modules/**"] }
 ]);
